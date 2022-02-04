57ad03c5e4f04ad7bfbf9aafbbe8abe9680cb253
flapjackstan <elm.s.camargo@gmail.com>
2022-02-04 13:00:57 -0800

    adding normal/gaussian distribution

--- distributions.r
+++ distributions.r
@@ -77,4 +77,127 @@ dunif(x=11, min=0, max=10)
 
 ###################################   Normal/Gaussian Distribution   ##########################################
 
+# Gaussian dist is characterized by its mean and sd. 
+# There is no explicit range of values like in the uniform dist,
+# but spread is dictated by the sd you provide it
+norm_data <- rnorm(N, mean=0, sd=1)
+head(norm_data,25)
 
+min(norm_data)
+max(norm_data)
+mean(norm_data)
+sd(norm_data)
+var(norm_data)
+
+hist(norm_data, breaks =  51, col = "orange", main = "runif")
+plot(norm_data, type = "p")
+
+plot(density(norm_data,kernel="gaussian"))
+
+norm_frame = with(density(norm_data,kernel="gaussian"),  # Create data frame density values
+                   data.frame(x,y))  
+
+
+norm_plot <- ggplot(data = norm_frame, aes(x = x, y = y)) +   # Create the plot
+  geom_line(color="NA")+
+  geom_ribbon(data=subset(norm_frame,x > -5 & x < 5),
+              aes(ymax=y, ymin=0),
+              fill="skyblue", 
+              alpha=0.4)+
+  xlim(-4,4)
+
+
+norm_plot
+
+# bulk of observations are clustered around the mean making a bell shape curve
+# roughly 68% of obs lie within 1 sd, 95% within 2 sd, and 99.7% within 3
+
+# visually seeing this
+
+norm_mean <- mean(norm_data)
+norm_sd <- sd(norm_data)
+
+plus_one_sd <-  norm_mean+norm_sd
+minus_one_sd <-  norm_mean-norm_sd
+
+plus_two_sd <- norm_mean+norm_sd*2
+minus_two_sd <- norm_mean-norm_sd*2
+
+plus_three_sd <- norm_mean+norm_sd*3
+minus_three_sd <- norm_mean-norm_sd*3
+
+norm_plot_w_m_and_sd <- norm_plot + 
+    geom_ribbon(data=subset(norm_frame,x > minus_three_sd & x < plus_three_sd),
+              aes(ymax=y, ymin=0),
+              fill="red", 
+              alpha=0.4)+
+  geom_ribbon(data=subset(norm_frame,x > minus_two_sd & x < plus_two_sd),
+              aes(ymax=y, ymin=0),
+              fill="yellow", 
+              alpha=0.4)+
+  geom_ribbon(data=subset(norm_frame,x > minus_one_sd & x < plus_one_sd),
+              aes(ymax=y, ymin=0),
+              fill="green", 
+              alpha=0.4)+
+  geom_vline(xintercept = norm_mean, color="black")
+  
+norm_plot_w_m_and_sd
+
+# calculating this, note abs(q) is the sd to make it easier
+
+# probability of getting a value less than -1
+pnorm(q=-1, mean=0, sd=1)
+# probability of getting a value more than -1
+1-pnorm(q=-1, mean=0, sd=1)
+
+# probability of getting a value less than 1
+pnorm(q=1, mean=0, sd=1)
+# probability of getting a value more than 1
+1-pnorm(q=1, mean=0, sd=1)
+
+# Get prob of observing a value less than -1
+prob_under_minus1 <- pnorm(q=-1, mean=0, sd=1)
+
+# Get prob of observing a value over 1
+prob_over_1 <-  1-pnorm(q=1, mean=0, sd=1)
+
+# Prob between -1 and 1, aka 1 sd
+between_prob <- 1-(prob_under_minus1+prob_over_1)  
+
+norm_plot_w_calc <- norm_plot + 
+  geom_ribbon(data=subset(norm_frame,x < minus_one_sd),
+              aes(ymax=y, ymin=0),
+              fill="red", 
+              alpha=0.4)+
+  geom_ribbon(data=subset(norm_frame,x > minus_one_sd),
+              aes(ymax=y, ymin=0),
+              fill="red", 
+              alpha=0.4)+
+  geom_ribbon(data=subset(norm_frame,x > minus_one_sd & x < plus_one_sd),
+              aes(ymax=y, ymin=0),
+              fill="green", 
+              alpha=0.4)+
+  geom_text(x=-1.6,y=0.03,label=round(prob_under_minus1,4),size=4) +
+  geom_text(x=1.6,y=0.03,label=round(prob_under_minus1,4),size=4) +
+  geom_text(x=0,y=0.1,label=round(1-(prob_under_minus1*2),4),size=5) 
+
+norm_plot_w_calc
+
+
+qnorm(p = .99, mean=0, sd=1) # 99% chance to randomly select an obs lower than this
+qnorm(p = 0.75,mean=0, sd=1) # 75% chance to randomly select a obs lower than this
+qnorm(p = 0.5,mean=0, sd=1) # 50% chance to randomly select a obs lower than this
+qnorm(p = 0.25,mean=0, sd=1) # 25% chance to randomly select a obs lower than this
+qnorm(p = .01,mean=0, sd=1) # 1% chance to randomly select a obs lower than this
+
+# 2.5% left tail
+left_cutoff <- qnorm(p = 0.025,mean=0, sd=1) # 2.5% chance to randomly select a obs lower than this
+# 2.5% right tail
+right_cutoff <- qnorm(p = 0.975,mean=0, sd=1) # 97.5% chance to randomly select a obs lower than this
+
+# roughly 5% of data fall outside of these cutoffs (aka outside 2nd sd)
+norm_plot_w_calc + 
+  geom_vline(xintercept = left_cutoff, color="black")+
+  geom_vline(xintercept = right_cutoff, color="black")
+
+###################################   Binomial/Bernouli Distribution   ##########################################
